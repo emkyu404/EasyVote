6e95723501bc3a7b5d727f145558a7e764a860bb
@Jean-Franck <jeanfranck.tang@gmail.com>
2021-06-14 10:06:04 +0200

    first

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {4+4}
         </a>
       </header>
     </div>
