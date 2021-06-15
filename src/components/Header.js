d9fb423f2ebef07d7bdff4b81ac6c371de8b4678
amadeuszzz <thierry93160@hotmail.com>
2021-06-15 11:21:57 +0200

    Clean

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Logo from '../img/logo.PNG'
+
+const Header = ()=>{
+    return(
+        <header className='header'>
+            <img src={Logo}></img>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Menu.js
@@ -0,0 +1,18 @@
+const Menu = ()=>{
+    return(
+        <nav class="nav-horizontal">
+            <div class="mask">
+                <ul class="list">
+                    <li><a href="#">Alpha</a></li>
+                    <li><a href="#">Beta</a></li>
+                    <li><a href="#">Gamma</a></li>
+                    <li><a href="#">Delta</a></li>
+                    <li><a href="#">Epsilon</a></li>
+                    <li><a href="#">Zeta</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Menu
