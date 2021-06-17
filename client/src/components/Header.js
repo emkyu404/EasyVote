08480d4f69c6475022d7dc6cb647b5c142e146d4
Antochg <56629942+Antochg@users.noreply.github.com>
2021-06-17 16:24:20 +0200

    header à moitié

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,20 @@
+import logo from '../img/header-logo.png';
+import {
+    BrowserRouter as Router,
+    Link
+} from "react-router-dom";
+
 const Header = () => {
     return (
         <div className='header'>
+            {/* <Router>
+                <Link className='header-link' to="/"> */}
+                    <img className='header-logo' src={logo}/>
+                {/* </Link>
+            </Router> */}
+
+            <button className='log-btn'>Se connecter</button>
+            <button className='log-btn'>Se déconnecter</button>
         </div>
     )
 }
