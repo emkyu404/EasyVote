e7b46a6781466559fd9ba96d8cbb453fc91c92c2
emkyu404 <contact.buiminhquan@gmail.com>
2021-06-17 11:25:55 +0200

    Remplacement du menu par un component Router, installer en utilisant npm install react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import "./css/App.css";
 import { useState } from "react";
 import Axios from "axios";
-import Menu from './components/Menu'
 import Header from './components/Header'
+import Home from './components/Home'
 import { Sling as Hamburger } from 'hamburger-react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [citizens, setCitizens] = useState([""])
@@ -12,16 +18,6 @@ function App() {
 
   const toggleMenu = () => {
     setShowMenu(!showMenu)
-  }
-
-  const useEffect = () => {
-    if(showMenu){
-      setTimeout(() => {
-        setRender(true)
-      }, 2000)
-    } else{
-      setRender(false)
-    }
   }
 
   const getCitizens = () => {
@@ -33,12 +29,31 @@ function App() {
 
   return (
     <div className="App">
+      {/* Header */}
       <Header />
+
+       {/* Tout ce qu'il y a sous la page */}
       <div className="flex-row">
-        {showMenu? <Menu /> : ''}
-        <Hamburger onToggle={() => {
-          toggleMenu()
-        }}  />
+          <Router>
+            {/* Si le state showMenu vrai, affiche le menu */}
+          {showMenu? <div className="menu-container">
+              <div className="menu-item">
+                <Link to="/" style={{ textDecoration : "none" }}>Home</Link>
+              </div>
+             </div> : ''}
+          {/* Toujours visible, change le component afficher en fonction de l'adresse correspondante (par défaut '/' correspond au component Home) */}
+            <div className="page">
+              <Hamburger onToggle={() => {
+                toggleMenu()
+              }}  />
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+        </Router>
+       
       </div>
     </div>
   );
