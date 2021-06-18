f88ba7d309ab37b5f69178b8dd97e44bad890258
Antochg <56629942+Antochg@users.noreply.github.com>
2021-06-18 10:41:04 +0200

    commit update logins

--- client/src/components/LoginAdmin.js
+++ client/src/components/LoginAdmin.js
@@ -1,15 +1,21 @@
 const LoginAdmin = () => {
+    const handleSubmit() = {
+
+    }
+
     return (
         <div>
-            <h1 class="login-title" style={loginTitleStyle}>Connexion administrateur</h1>
+            <form onSubmit={this.han}>
+                <h1 class="login-title" style={loginTitleStyle}>Connexion administrateur</h1>
 
-            <label class="login-label" for="email" style={loginLabelStyle}>Votre email : </label>
-            <input class="login-input" type="email" id="email" name="email" style={loginInputStyle} required />
+                <label class="login-label" for="email" style={loginLabelStyle}>Votre email : </label>
+                <input class="login-input" type="email" id="email" name="email" style={loginInputStyle} required />
 
-            <label class="login-label" for="password" style={loginLabelStyle}>Votre mot de passe : </label>
-            <input class="login-input" type="password" id="password" name="password" style={loginInputStyle} required />
+                <label class="login-label" for="password" style={loginLabelStyle}>Votre mot de passe : </label>
+                <input class="login-input" type="password" id="password" name="password" style={loginInputStyle} required />
 
-            <button class="login-button">Se connecter</button>
+                <button class="login-button">Se connecter</button>
+            </form>
         </div>
     )
 }
--- client/src/components/LoginUser.js
+++ client/src/components/LoginUser.js
@@ -1,15 +1,17 @@
 const LoginUser = () => {
     return (
         <div>
-            <h1 class="login-title" style={loginTitleStyle}>Connexion</h1>
+            <form>
+                <h1 class="login-title" style={loginTitleStyle}>Connexion</h1>
 
-            <label class="login-label" for="email" style={loginLabelStyle}>Votre email : </label>
-            <input class="login-input" type="email" id="email" name="email" style={loginInputStyle} required />
+                <label class="login-label" for="email" style={loginLabelStyle}>Votre email : </label>
+                <input class="login-input" type="email" id="email" name="email" style={loginInputStyle} required />
 
-            <label class="login-label" for="password" style={loginLabelStyle}>Votre mot de passe : </label>
-            <input class="login-input" type="password" id="password" name="password" style={loginInputStyle} required />
+                <label class="login-label" for="password" style={loginLabelStyle}>Votre mot de passe : </label>
+                <input class="login-input" type="password" id="password" name="password" style={loginInputStyle} required />
 
-            <button class="login-button">Se connecter</button>
+                <button class="login-button">Se connecter</button>
+            </form>
         </div>
     )
 }
