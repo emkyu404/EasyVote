59a3db2ba56cdb15f580fadf5d4e18eb219b457e
Antochg <56629942+Antochg@users.noreply.github.com>
2021-06-21 16:54:30 +0200

    html AddElection

--- client/src/components/AddElection.js
+++ client/src/components/AddElection.js
@@ -5,7 +5,7 @@ const AddElection = () => {
     const [electionType, setElectionType] = useState("")
 
     const handleChange = (e) => {
-        if(electionType !== "null") {
+        if(electionType !== undefined) {
             setElectionType(e.target.value)
         }
     }
@@ -13,15 +13,16 @@ const AddElection = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(electionType)
+        // AJOUTER LA FONCTION BACK
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <h2>Ajouter une nouvelle élection</h2>
+            <h2 className="add-election-title">Ajouter une nouvelle élection</h2>
 
             <label className="add-election-label" style={addElectionLabel}>Type de l'élection : 
-                <select id="electionType" value={electionType.value} defaultValue={"null"} onChange={handleChange}>
-                    <option value="">Sélectionner un type</option>
+                <select id="electionType" value={electionType.value} defaultValue={""} onChange={handleChange}>
+                    <option value="" selected="selected">Sélectionner un type</option>
                     <option value="election_nationale">Election nationale</option>
                     <option value="election_regionale">Election régionale</option>
                     <option value="election_departementale">Election départementale</option>
@@ -29,28 +30,45 @@ const AddElection = () => {
                 </select>
             </label>
 
-            {/* if elections */}
-            <label className="add-election-label" style={addElectionLabel}>Titre de l'élection : 
-                <input type="text" required />
-            </label>
+            {electionType !== "" &&
+                <div>
+                    {electionType === "election_regionale" &&
+                        <label className="add-election-label" style={addElectionLabel}>Nom de la région : 
+                            <input type="text" required />
+                        </label>
+                    }
 
-            <label className="add-election-label" style={addElectionLabel}>Titre de l'élection : 
-                <input type="text" required />
-            </label>
+                    {electionType === "election_departementale" &&
+                        <label className="add-election-label" style={addElectionLabel}>Code du département : 
+                            <input type="text" required />
+                        </label>
+                    }
 
-            <label className="add-election-label" style={addElectionLabel}>Date de début : 
-                <input type="date" required />
-            </label>
+                    {electionType === "election_municipale" &&
+                        <label className="add-election-label" style={addElectionLabel}>Code postal : 
+                            <input type="text" required />
+                        </label>
+                    }
 
-            <label className="add-election-label" style={addElectionLabel}>Date de fin : 
-                <input type="date" required />
-            </label>
+                    <label className="add-election-label" style={addElectionLabel}>Titre de l'élection : 
+                        <input type="text" required />
+                    </label>
 
-            <label className="add-election-label" style={addElectionLabel}>Description de l'élection : 
-                <textarea type="text" required></textarea>
-            </label>
+                    <label className="add-election-label" style={addElectionLabel}>Date de début : 
+                        <input type="date" required />
+                    </label>
 
-            <input type="submit" value="Ajouter" />
+                    <label className="add-election-label" style={addElectionLabel}>Date de fin : 
+                        <input type="date" required />
+                    </label>
+
+                    <label className="add-election-label" style={addElectionLabel}>Description de l'élection : 
+                        <textarea type="text" required></textarea>
+                    </label>
+
+                    <input type="submit" value="Ajouter" />
+                </div>
+            }
         </form>
     )
 }
