7ca940921b2f547d8b267664a11686b8e6240687
Antochg <56629942+Antochg@users.noreply.github.com>
2021-06-22 09:54:10 +0200

    raduim CSS

--- client/src/components/BoutonConnexionDeconnexion.js
+++ client/src/components/BoutonConnexionDeconnexion.js
@@ -1,28 +1,36 @@
+import Radium from 'radium';
+
 const BoutonConnexionDeconnexion = ({ onDisconnection, isConnected }) => {
     return (
-        <div className='button-container' style={buttonContainerStyle}>
+        <div className='button-container' style={styles.buttonContainerStyle}>
         {isConnected? 
-            <button className='log-btn' style={BoutonConnexionDeconnexionStyle} onClick={onDisconnection}>Se déconnecter</button>:
-            <a href="/login"><button className='log-btn' style={BoutonConnexionDeconnexionStyle}>Se connecter</button></a>
+            <button className='log-btn' style={styles.boutonConnexionDeconnexionStyle} onClick={onDisconnection}>Se déconnecter</button>:
+            <a href="/login"><button className='log-btn' style={styles.boutonConnexionDeconnexionStyle}>Se connecter</button></a>
         }
         </div>
     )
 }
 
-const BoutonConnexionDeconnexionStyle = {
-    position : "relative",
-    backgroundColor: "#0B6BA8",
-    padding : "15px",
-    top : "50%",
-    transform : "translateY(-50%)",
-    border : "none",
-    color : "white",
-    cursor : "pointer"
+const styles = {
+    boutonConnexionDeconnexionStyle: {
+        position : "relative",
+        backgroundColor: "#0B6BA8",
+        padding : "15px",
+        top : "50%",
+        transform : "translateY(-50%)",
+        border : "none",
+        color : "white",
+        cursor : "pointer",
+        ':hover': {
+            color: "#0B6BA8",
+            backgroundColor: "red"
+        }
+    },
+    buttonContainerStyle: {
+        height : "100%",
+        marginRight : "5vw"
+    }
 }
 
-const buttonContainerStyle = {
-    height : "100%",
-    marginRight : "5vw"
-}
 
-export default BoutonConnexionDeconnexion
+export default Radium(BoutonConnexionDeconnexion)
