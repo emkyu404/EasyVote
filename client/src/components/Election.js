0475a120b9d5f973738c4bbf99f7505d3fb60da3
Minh-Quân Bui <contact.buiminhquan@gmail.com>
2021-06-24 16:31:41 +0200

    suppression du contenu provisoire de Election.js

--- client/src/components/Election.js
+++ client/src/components/Election.js
@@ -1,57 +1,10 @@
 import React from 'react'
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from '@material-ui/core/Slide';
-import Button from '@material-ui/core/Button'
-
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-
 const Election = () => {
-    /* utile */
-    const [open, setOpen] = React.useState(false);
-    const [dialogText, setDialogText] = React.useState("Souhaitez-vous confirmer votre choix pour le candidat n°X ?")
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
 
     return (
         <div>
-            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                Slide in alert dialog
-            </Button>
-            <Dialog
-                open={open}
-                TransitionComponent={Transition}
-                keepMounted
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-slide-title"
-                aria-describedby="alert-dialog-slide-description"
-            >
-                <DialogTitle id="alert-dialog-slide-title">{"Confirmation du vote"}</DialogTitle>
-                <DialogContent>
-                <DialogContentText id="alert-dialog-slide-description">
-                    {dialogText}
-                </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                <Button onClick={handleClose} color="primary">
-                    Oui
-                </Button>
-                <Button onClick={handleClose} color="primary">
-                    Non
-                </Button>
-                </DialogActions>
-            </Dialog>
+        
         </div>
     )
 }
