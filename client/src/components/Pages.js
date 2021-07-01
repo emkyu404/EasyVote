4aae65c30cf608cfe2aabc2c2fe7db8f2790095c
amaurylin <amaury.lin@hotmail.fr>
2021-07-01 17:43:19 +0200

    nvl page mdp oublié

--- client/src/components/Pages.js
+++ client/src/components/Pages.js
@@ -12,56 +12,60 @@ import NotConnected from './NotConnected'
 import Election from './Election'
 import AddElection from "./AddElection"
 import PageNotFound from './PageNotFound'
+import PasswordForget from './PasswordForget'
 
-const Pages = ({connected, idElection, addCandidat, addElection, getElections, getCurrentDate, filteredElections, filterElection, profile, currentUser, login, loginAdmin, getElection, election, getCandidats, candidats}) => {
-    return(
-        <div className="main-container default-margin" style={styles.mainContainer}>
-            <Switch>
-              <Route exact path="/">
-                <Home pageTitle={'Accueil'}/>
-              </Route>
-              <Route exact path="/addElection">
-                <AddElection idElection={idElection} addCandidat={addCandidat} onAddElection={addElection} pageTitle={'Ajout d\'une élection'}/>
-                {/* {connected ? <AddElection idElection={idElection} addCandidat={addCandidat} onAddElection={addElection} pageTitle={'Ajout d\'une élection'}/> : <NotConnected />} */}
-              </Route>
-              <Route exact path="/elections">
-                {connected ? <Elections getElections={getElections} getCurrentDate={getCurrentDate} filteredElections={filteredElections} filterElection={filterElection} pageTitle={'Elections'}/> : <NotConnected />}
-              </Route>
-              <Route exact path="/profil">
-                {connected ? <Profil getProfile={profile} currentUser={currentUser} pageTitle={'Profil'} /> : <NotConnected />}
-              </Route>
-              <Route exact path="/contact">
-                <Contact pageTitle={'Contactez nous'}/>
-              </Route>
-              <Route exact path="/login">
-                {connected ? <Home pageTitle={'Accueil'}/> : <Login onLogin={login} pageTitle={'Connexion'}/>}
-              </Route>
-              <Route exact path="/loginAdmin">
-                {connected ? <Home pageTitle={'Accueil'}/> : <LoginAdmin onLogin={loginAdmin} pageTitle={'Connexion administrateur'}/>}
-              </Route>
-              <Route path="/election/:idElection">
-                {connected ?<Election getElection={getElection} election={election} getCandidats={getCandidats} candidats={candidats} pageTitle={'Election'}/> : <NotConnected />}
-              </Route>
-              
-              <Route path="*" >
-                <PageNotFound pageTitle={'404 - Not Found'}/>
-              </Route>
-            </Switch>
-          </div>
-    )
+const Pages = ({ connected, idElection, addCandidat, addElection, getElections, getCurrentDate, filteredElections, filterElection, profile, currentUser, login, loginAdmin, getElection, election, getCandidats, candidats }) => {
+  return (
+    <div className="main-container default-margin" style={styles.mainContainer}>
+      <Switch>
+        <Route exact path="/">
+          <Home pageTitle={'Accueil'} />
+        </Route>
+        <Route exact path="/addElection">
+          <AddElection idElection={idElection} addCandidat={addCandidat} onAddElection={addElection} pageTitle={'Ajout d\'une élection'} />
+          {/* {connected ? <AddElection idElection={idElection} addCandidat={addCandidat} onAddElection={addElection} pageTitle={'Ajout d\'une élection'}/> : <NotConnected />} */}
+        </Route>
+        <Route exact path="/elections">
+          {connected ? <Elections getElections={getElections} getCurrentDate={getCurrentDate} filteredElections={filteredElections} filterElection={filterElection} pageTitle={'Elections'} /> : <NotConnected />}
+        </Route>
+        <Route exact path="/profil">
+          {connected ? <Profil getProfile={profile} currentUser={currentUser} pageTitle={'Profil'} /> : <NotConnected />}
+        </Route>
+        <Route exact path="/contact">
+          <Contact pageTitle={'Contactez nous'} />
+        </Route>
+        <Route exact path="/login">
+          {connected ? <Home pageTitle={'Accueil'} /> : <Login onLogin={login} pageTitle={'Connexion'} />}
+        </Route>
+        <Route exact path="/loginAdmin">
+          {connected ? <Home pageTitle={'Accueil'} /> : <LoginAdmin onLogin={loginAdmin} pageTitle={'Connexion administrateur'} />}
+        </Route>
+        <Route path="/election/:idElection">
+          {connected ? <Election getElection={getElection} election={election} getCandidats={getCandidats} candidats={candidats} pageTitle={'Election'} /> : <NotConnected />}
+        </Route>
+        <Route exact path="/passwordForget">
+          <PasswordForget pageTitle={'Mot de passe oublié'} />
+        </Route>
+
+        <Route path="*" >
+          <PageNotFound pageTitle={'404 - Not Found'} />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 const styles = {
-  mainContainer : {
-    marginTop : '15vh',
-    maxWidth : '1200px',
-    width : '80%',
-    backgroundColor : '#E9F1F7',
-    padding : '25px',
-    minHeight : 'calc(85vh - 50px)',
-    height : 'fit-content',
-    '@media (max-width :640px)':{
-      width : '100%'
+  mainContainer: {
+    marginTop: '15vh',
+    maxWidth: '1200px',
+    width: '80%',
+    backgroundColor: '#E9F1F7',
+    padding: '25px',
+    minHeight: 'calc(85vh - 50px)',
+    height: 'fit-content',
+    '@media (max-width :640px)': {
+      width: '100%'
     }
   }
 }
---
+++ client/src/components/PasswordForget.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+
+const PasswordForget = () => {
+
+    const [email, setEmail] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const handleEmailOnChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    return (
+        <div style={{ width: "100%" }}>
+            <form onSubmit={handleSubmit} style={styles.formStyle}>
+                <h1 className="forget-title" style={styles.forgetTitle}>Mot de passe oublié</h1>
+
+                <label className="forget-label" style={styles.forgetLabel}>Votre email : </label>
+                <input className="forget-input" type="email" id="email" name="email" style={styles.forgetInput} required onBlur={handleEmailOnChange} />
+
+                <input type="submit" className="forget-button" value="Envoyer" style={styles.forgetSubmit} />
+
+            </form>
+        </div>
+    )
+}
+
+const styles = {
+    formStyle: {
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        maxWidth: "300px",
+        margin: "auto"
+    },
+
+    forgetTitle: {
+        fontSize: "20px",
+        textAlign: "center",
+        marginBottom: "20px"
+    },
+
+    forgetInput: {
+        padding: "5px",
+        marginTop: "5px"
+    },
+
+    forgetSubmit: {
+        padding: "15px",
+        marginTop: "20px",
+        backgroundColor: "#0B6BA8",
+        border: "none",
+        color: "white",
+        cursor: "pointer",
+        ':hover': {
+            backgroundColor: "#074E7B",
+            transition: "0.2s"
+        }
+    },
+
+    forgetLabel: {
+        float: "left",
+        height: "50px",
+        lineHeight: "50px",
+        textAlign: "center",
+        verticalAlign: "middle",
+        '@media (max-width: 920px)': {
+            float: "none",
+            lineHeight: "30px"
+        }
+    }
+}
+
+export default PasswordForget
