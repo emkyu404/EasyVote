f3fcafad4d723041fce86a432f67258b40b8db86
amaurylin <amaury.lin@hotmail.fr>
2021-07-06 01:18:35 +0200

    espacement des input login

--- client/src/components/LoginAdmin.js
+++ client/src/components/LoginAdmin.js
@@ -24,49 +24,50 @@ const LoginAdmin = ({ onLogin, pageTitle }) => {
     }
 
     return (
-        <div style={{width : "100%"}}>
+        <div style={{ width: "100%" }}>
             <form onSubmit={handleSubmit} style={styles.formStyle}>
-            <h1 className="login-title" style={styles.loginTitleStyle}>Connexion administrateur</h1>
+                <h1 className="login-title" style={styles.loginTitleStyle}>Connexion administrateur</h1>
 
                 <label className="login-label" style={styles.loginLabelStyle}>Votre email : </label>
                 <input className="login-input" type="email" id="email" name="email" style={styles.loginInputStyle} required onBlur={handleEmailOnChange} />
-                
+
                 <label className="login-label" style={styles.loginLabelStyle}>Votre mot de passe : </label>
                 <input className="login-input" type="password" id="password" name="password" style={styles.loginInputStyle} required onBlur={handlePasswordOnChange} />
-                
-                <input type="submit" className="login-button" value="Se connecter" style={styles.loginSubmitStyle}/>
+
+                <input type="submit" className="login-button" value="Se connecter" style={styles.loginSubmitStyle} />
             </form>
         </div>
     )
 }
 
 const styles = {
-    formStyle : {
-    display : "flex",
-    flexDirection : "column",
-    width : "100%",
-    maxWidth :"300px",
-    margin : "auto"
+    formStyle: {
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        maxWidth: "300px",
+        margin: "auto"
     },
 
-    loginTitleStyle : {
-        fontSize : "20px",
-        textAlign : "center",
-        marginBottom : "20px"
+    loginTitleStyle: {
+        fontSize: "20px",
+        textAlign: "center",
+        marginBottom: "20px"
     },
 
-    loginInputStyle : {
-        padding : "5px",
-        marginTop : "5px"
+    loginInputStyle: {
+        padding: "5px",
+        marginTop: "5px",
+        marginBottom: "15px",
     },
 
-    loginSubmitStyle : {
-        padding : "15px",
-        marginTop : "20px",
+    loginSubmitStyle: {
+        padding: "15px",
+        marginTop: "10px",
         backgroundColor: "#0B6BA8",
-        border : "none",
-        color : "white",
-        cursor : "pointer",
+        border: "none",
+        color: "white",
+        cursor: "pointer",
         ':hover': {
             backgroundColor: "#074E7B",
             transition: "0.2s"
