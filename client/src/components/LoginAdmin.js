ac3a74c51ce39e0d349d6d928dc2eccd5e413c4b
@Jean-Franck <jeanfranck.tang@gmail.com>
2021-07-06 15:03:17 +0200

    login bug fix + suppression de code inutile

--- client/src/components/LoginAdmin.js
+++ client/src/components/LoginAdmin.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Radium from 'radium'
 
+
 const LoginAdmin = ({ onLogin, pageTitle }) => {
 
     useEffect(() => {
@@ -10,12 +11,8 @@ const LoginAdmin = ({ onLogin, pageTitle }) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleEmailOnChange = (e) => {
-        setEmail(e.target.value)
-    }
-
-    const handlePasswordOnChange = (e) => {
-        setPassword(e.target.value)
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
     }
 
     const handleSubmit = (e) => {
@@ -24,17 +21,17 @@ const LoginAdmin = ({ onLogin, pageTitle }) => {
     }
 
     return (
-        <div style={{width : "100%"}}>
+        <div style={{ width : "100%" }}>
             <form onSubmit={handleSubmit} style={styles.formStyle}>
-            <h1 className="login-title" style={styles.loginTitleStyle}>Connexion administrateur</h1>
+                <h1 className="login-title" style={styles.loginTitleStyle}>Connexion administrateur</h1>
 
                 <label className="login-label" style={styles.loginLabelStyle}>Votre email : </label>
-                <input className="login-input" type="email" id="email" name="email" style={styles.loginInputStyle} required onBlur={handleEmailOnChange} />
+                <input className="login-input" type="email" id="email" name="email" style={styles.loginInputStyle} required onChange={(e) => setEmail(e.target.value)} />
                 
                 <label className="login-label" style={styles.loginLabelStyle}>Votre mot de passe : </label>
-                <input className="login-input" type="password" id="password" name="password" style={styles.loginInputStyle} required onBlur={handlePasswordOnChange} />
+                <input className="login-input" type="password" id="password" name="password" style={styles.loginInputStyle} required onChange={(e) => setPassword(e.target.value)} />
                 
-                <input type="submit" className="login-button" value="Se connecter" style={styles.loginSubmitStyle}/>
+                <input type="submit" className="login-button" value="Se connecter" style={styles.loginSubmitStyle} disabled={!validateForm()}/>
             </form>
         </div>
     )
